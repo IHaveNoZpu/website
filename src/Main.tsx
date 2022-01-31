8d204f0546535ed3d62eeaf4e4300b8761f650ee
IHaveNoZpu <ihavenozpu.fui@gmail.com>
2022-01-31 16:58:32 +0700

    Setup Project

---
+++ src/Main.tsx
@@ -0,0 +1,13 @@
+// Import \\
+import React from "react"
+import ReactDOM from "react-dom"
+
+import App from "./App"
+
+// Render \\
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
+)
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+// Imports \\
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+
+// Export \\
+export default defineConfig({
+	plugins: [react()]
+})
